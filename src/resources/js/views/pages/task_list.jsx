aba6245c209d9c39819620045e677b46479f2bc5
HishidaTatsuki <89442937+HishidaTatsuki@users.noreply.github.com>
2024-06-26 17:56:17 +0900

    [タスク] delete処理を変更

--- src/resources/js/views/pages/task_list.jsx
+++ src/resources/js/views/pages/task_list.jsx
@@ -1,7 +1,5 @@
 import { React, useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import { useInputState, getHotkeyHandler, useDisclosure } from '@mantine/hooks';
 import { useParams, Link } from "react-router-dom";
 import { Checkbox, Button, Text } from '@mantine/core';
 import {
@@ -10,16 +8,14 @@ import {
   MultiBackend,
 } from "@minoru/react-dnd-treeview";
 import { DndProvider } from "react-dnd";
-import { unwrapResult } from '@reduxjs/toolkit';
 import TaskAddModal from '../components/addTaskModal';
+import DeleteTaskForm from '../components/deleteTaskForm';
 import DoneTaskForm from '../components/doneTaskForm';
 import VariableLengthText from '../components/VariableLengthText';
-import { getTasks, deleteTask } from '../../state/reducks/tasks/slices';
 
 function TaskList() {
   const handleDrop = (newTaskData) => setTasks(newTaskData);
   const { categoryId } = useParams();
-  const dispatch = useDispatch();
 
   const queryClient = useQueryClient();
   let categories = queryClient.getQueryData(['categories']);
@@ -64,27 +60,6 @@ function TaskList() {
   if (isError) {
     return <h2>{error.message}</h2>;
   }
-
-  // タスクを削除する
-  const handleTrashButton = async (event, id) => {
-    event.preventDefault();
-
-    // フォームデータを作成
-    const data = new FormData();
-    data.append("id", id);
-
-    try {
-      const result = await dispatch(deleteTask(data));
-
-      if (unwrapResult(result) !== false) {
-        console.log('delete done');
-      } else {
-        console.error('Failed to add the task');
-      }
-    } catch (err) {
-      console.error('Failed to save the post: ', err)
-    }
-  };
 
   return (
     <div>
@@ -132,11 +107,7 @@ function TaskList() {
               <DoneTaskForm taskId={node.id} categoryId={categoryId} isDone={node.done} text={node.text} key={node.id}/>
               <div className={`rounded-md duration-200 ${node.done ? 'hover:bg-red-500' : 'hover:bg-teal-50'}`}>{
                 node.done
-                ? <span className='text-red-500 hover:text-white duration-200' onClick={(event) => handleTrashButton(event, node.id)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                    </svg>
-                  </span>
+                ? <DeleteTaskForm taskId={node.id} categoryId={categoryId} key={node.id}/>
                 : <TaskAddModal parentId={node.id} categoryId={categoryId} key={node.id}/>
               }</div>
             </div>
